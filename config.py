45c3cc9951e53e773c677e740a05cc47e0257b1c
Yarikfonarik <108357247+Yarikfonarik@users.noreply.github.com>
2026-03-30 11:46:59 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
-BOT_TOKEN="7607684238:AAE4vzPPHSBKwR5qzyQHpn_s1cucUCjfDqI"
-DATABASE_URL="postgresql://postgres:aaa77755@localhost:5433/student_bot"
-ADMIN_IDS={593577422}
+import os
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+DATABASE_URL = os.environ.get("DATABASE_URL", "")
+ADMIN_IDS = set(map(int, os.environ.get("ADMIN_IDS", "593577422").split(",")))
